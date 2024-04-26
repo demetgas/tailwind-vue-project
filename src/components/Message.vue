e89d9ac7313d6574d91a46bcd6c2ec18acc150f5
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 16:46:14 +0200

    message

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,8 +1,67 @@
 <template>
-  <div>Hello</div>
+  <div className="max-w-[1140px] m-auto mt-[5svh] ">
+    <h2 className="text-center text-gray-700 p-4 font-bold text-4xl">
+      Send us a message
+    </h2>
+    <p className="text-center text-gray-700 text-2xl">
+      Can't wait to hear your thoughts!
+    </p>
+    <div className="grid md:grid-cols-2 mt-16 ">
+      <img
+        :src="f2"
+        alt="/"
+        className=" md:h-full sm:w-[400px] sm:h-[400px] sm:mx-auto  p-2 max-h-[500px] h-[200px] w-[420px] hover:scale-110 transition-all delay-75"
+      />
+      <form>
+        <div className="grid grid-cols-2  ">
+          <input
+            className="border m-2 p-2 rounded"
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            className="border m-2 p-2 rounded"
+            type="text"
+            placeholder="Last Name"
+          />
+          <input
+            className="border m-2 p-2 rounded"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className="border m-2 p-2 rounded"
+            type="tel"
+            placeholder="Phone"
+          />
+          <input
+            className="border col-span-2 p-2 m-2 rounded"
+            type="text"
+            placeholder="Address"
+          />
+          <textarea
+            className="border col-span-2 m-2 p-2 rounded"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button
+            className="m-2 p-2 col-span-2 rounded text-white bg-hero2 hover:bg-hero"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 <script>
+import f2 from "../assets/f2.jpeg";
 export default {
   name: "MessageSection",
+  data() {
+    return {
+      f2: f2,
+    };
+  },
 };
 </script>
