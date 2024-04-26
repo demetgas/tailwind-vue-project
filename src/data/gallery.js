2edffb7ebead92221348810caed92e6a42e27925
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 16:30:16 +0200

    gallery created

---
+++ src/data/gallery.js
@@ -0,0 +1,65 @@
+import one from "../assets/1.jpeg";
+import two from "../assets/2.jpeg";
+import three from "../assets/3.jpeg";
+import four from "../assets/4.jpeg";
+import five from "../assets/5.jpeg";
+import six from "../assets/6.jpeg";
+import seven from "../assets/7.jpeg";
+import eight from "../assets/8.jpeg";
+import nine from "../assets/9.jpeg";
+import ten from "../assets/10.jpeg";
+import eleven from "../assets/11.jpeg";
+import twelve from "../assets/12.jpeg";
+
+const gallery1 = [
+  {
+    id: 1,
+    img: two,
+  },
+  {
+    id: 2,
+    img: seven,
+  },
+  {
+    id: 3,
+    img: six,
+  },
+  {
+    id: 4,
+    img: nine,
+  },
+  {
+    id: 5,
+    img: eleven,
+  },
+  {
+    id: 6,
+    img: four,
+  },
+  {
+    id: 7,
+    img: three,
+  },
+  {
+    id: 8,
+    img: eight,
+  },
+  {
+    id: 9,
+    img: one,
+  },
+  {
+    id: 10,
+    img: ten,
+  },
+  {
+    id: 11,
+    img: five,
+  },
+  {
+    id: 12,
+    img: twelve,
+  },
+];
+
+export default gallery1;
