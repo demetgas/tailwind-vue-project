9c09dea2b43b6f1c7d7e0b9aec33514b3399cea6
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 15:47:36 +0200

    created data

---
+++ src/data/cats.js
@@ -0,0 +1,28 @@
+import lili from "../assets/lili.jpeg";
+import freyja from "../assets/freyja.jpeg";
+import malkoc from "../assets/malkoc.jpeg";
+import nina from "../assets/nina.jpeg";
+
+const cats = [
+  {
+    id: 1,
+    name: "Lili",
+    img: lili,
+  },
+  {
+    id: 2,
+    name: "Nina",
+    img: nina,
+  },
+  {
+    id: 3,
+    name: "Malkoç",
+    img: malkoc,
+  },
+  {
+    id: 4,
+    name: "Freyja",
+    img: freyja,
+  },
+];
+export default cats;
