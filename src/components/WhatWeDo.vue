7eb49a1d1eada774641ce0d37e88be5466324b09
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 16:29:26 +0200

    whatwedo

--- src/components/WhatWeDo.vue
+++ src/components/WhatWeDo.vue
@@ -1,8 +1,89 @@
 <template>
-  <div>Channie</div>
+  <div className="max-w-[100%] lg:h-[30svh] md:h-[50svh] mt-20">
+    <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10">
+      <div
+        className="mx-auto border-2 m-10 lg:w-[20svw] md:w-[20svh] sm:w-[40svw] h-auto p-5 flex flex-col items-center hover:scale-110 transition-all delay-75 cursor-pointer"
+      >
+        <font-awesome-icon class="text-babyblue2" size="4x" :icon="faCat" />
+
+        <h2 className="text-center font-bold m-2">A Lovely Friend</h2>
+        <p className="text-center text-justify">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iusto sit
+          corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati
+          accusantium repellendus voluptatum reprehenderit deleniti cupiditate
+          ad fugit illo blanditiis nisi?
+        </p>
+      </div>
+      <div
+        className="mx-auto border-2 m-10 lg:w-[20svw] md:w-[280px] sm:w-[40svw] h-auto p-5 flex flex-col items-center hover:scale-110 transition-all delay-75 cursor-pointer"
+      >
+        <font-awesome-icon class="text-babyblue2" size="4x" :icon="faPaw" />
+        <h2 className="text-center font-bold m-2">A Lovely Place</h2>
+        <p className="text-center text-justify">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iusto sit
+          corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati
+          accusantium repellendus voluptatum reprehenderit deleniti cupiditate
+          ad fugit illo blanditiis nisi?
+        </p>
+      </div>
+      <div
+        className="mx-auto border-2 m-10 lg:w-[20svw] md:w-[280px] sm:w-[40svw] h-auto p-5 flex flex-col items-center hover:scale-110 transition-all delay-75 cursor-pointer"
+      >
+        <font-awesome-icon
+          class="text-babyblue2"
+          size="4x"
+          :icon="faBowlFood"
+        />
+
+        <h2 className="text-center font-bold m-2">A Lovely Meal</h2>
+        <p className="text-center text-justify">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iusto sit
+          corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati
+          accusantium repellendus voluptatum reprehenderit deleniti cupiditate
+          ad fugit illo blanditiis nisi?
+        </p>
+      </div>
+      <div
+        className="mx-auto border-2 m-10 lg:w-[20svw] md:w-[280px] sm:w-[40svw] h-auto p-5 flex flex-col items-center hover:scale-110 transition-all delay-75 cursor-pointer"
+      >
+        <font-awesome-icon class="text-babyblue2" size="4x" :icon="faHeart" />
+        <h2 className="text-center font-bold m-2">A Lovely Day</h2>
+        <p className="text-center text-justify">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iusto sit
+          corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati
+          accusantium repellendus voluptatum reprehenderit deleniti cupiditate
+          ad fugit illo blanditiis nisi?
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import {
+  faCat,
+  faPaw,
+  faBowlFood,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 export default {
   name: "WhatWeDo",
+  components: {
+    FontAwesomeIcon,
+  },
+  computed: {
+    faCat() {
+      return faCat;
+    },
+    faPaw() {
+      return faPaw;
+    },
+    faBowlFood() {
+      return faBowlFood;
+    },
+    faHeart() {
+      return faHeart;
+    },
+  },
 };
 </script>
