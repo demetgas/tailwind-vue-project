cc04c261d38b86072a480959a2569eca03d4ac60
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 16:02:34 +0200

    intro created

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>Hello</div>
+  <div
+    className="max-w-[100svw] mx-auto h-[30svh] bg-hero mt-64  text-white flex justify-center items-center"
+  >
+    <div className="text-center">
+      <h1 className="lg:text-4xl md:text-3xl sm:text-[27px]  font-bold ">
+        Welcome to our cat community, where our furry friends are here to
+        support you!
+      </h1>
+      <p className="text-2xl mt-8">
+        Join us to share in the joy and wonder that these charming creatures
+        bring to our lives.
+      </p>
+      <button
+        className="bg-babyblue hover:bg-babyblue2 rounded w-32 h-12 text-black font-bold mt-10"
+      >
+        Contact Us
+      </button>
+    </div>
+  </div>
 </template>
 <script>
 export default {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        babyblue: "#d4dfe7",
+        babyblue2: "#acbfcc",
+        hero: "#091315",
+        hero2: "#142529",
+        intro: "#141414",
+      },
+    },
   },
   variants: {
     extend: {},
