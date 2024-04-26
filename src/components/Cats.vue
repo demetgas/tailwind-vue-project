c13350da0e9a40883182be8c5a122ed4bdedc6cd
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 15:51:29 +0200

    added my cats

--- src/components/Cats.vue
+++ src/components/Cats.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>
-    <div class="text-red-500">Hello</div>
+  <div
+    className="max-w-[1140px] mx-auto w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-[-75px]"
+  >
+    <div
+      v-for="cat in cats"
+      className="relative p-4 hover:scale-110  transition-all delay-75 cursor-pointer text-center"
+      :key="cat.id"
+    >
+      <img
+        className="w-64 h-64 object-cover mx-auto border-4 border-black p-2 shadow-lg"
+        :src="cat.img"
+        alt="/"
+      />
+      <h3
+        className="text-black underline bg-opacity-70 rounded-md p-1.5 border-black text-xl font-bold"
+      >
+        {{ cat.name }}
+      </h3>
+    </div>
   </div>
 </template>
 <script>
+import cats from "../data/cats.js";
 export default {
   name: "CatSection",
+  data() {
+    return {
+      cats: cats,
+    };
+  },
 };
 </script>
