ba6820049cfc7619458618bac9f15229bff853d6
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 16:23:21 +0200

    info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>Hello</div>
+  <div
+    className="max-w-[100%] p-10 lg:mt-[650px] md:mt-[1000px] sm:mt-[180px]  bg-babyblue"
+  >
+    <h2 className="text-center text-gray-700 p-4  font-bold text-4xl italic">
+      Cats are fascinating creatures
+    </h2>
+    <div
+      className="grid lg:grid-cols-2 sm:grid-cols-1 gap-32 max-w-[1140px] mx-auto py-10"
+    >
+      <div className="text-justify">
+        <p className="mb-7 mt-10 text-lg">
+          Cats' unique behaviors, from their playful antics to their grooming
+          rituals, highlight their captivating nature. Despite their independent
+          streak, cats form strong bonds with their human companions, offering
+          companionship and warmth in a way that's truly special.
+        </p>
+        <p className="mb-10 text-lg">
+          One of the most captivating aspects of cats is their remarkable
+          agility and athleticism. Whether gracefully leaping to impossible
+          heights or contorting their bodies with impressive flexibility, cats
+          never cease to amaze with their acrobatic prowess. Their keen senses
+          and lightning-fast reflexes further contribute to their status as
+          skilled hunters and agile explorers of their environments.
+        </p>
+        <p className="text-lg">
+          Understanding and appreciating the intricacies of cat behavior not
+          only deepens our connection with these remarkable animals but also
+          enriches our own lives in immeasurable ways.
+        </p>
+      </div>
+      <div className="flex justify-center items-center">
+        <img
+          src="https://images.vexels.com/media/users/3/272056/isolated/preview/54b293603423b19ebd21fee4c7f475e8-cute-black-cartoon-cat.png"
+          alt=""
+          className="lg:w-[450px] md:w-[300px]   max-h-full hover:scale-110 transition-all delay-75"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
