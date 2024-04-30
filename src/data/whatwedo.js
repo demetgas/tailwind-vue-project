542fe0ccc0e95f441915243b8fd5661aab33f45d
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-30 10:52:23 +0200

    data changed

--- src/data/whatwedo.js
+++ src/data/whatwedo.js
@@ -9,25 +9,25 @@ const whatWeDo = [
     id: 1,
     icon: faCat,
     title: "A Lovely Friend",
-    desc: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iustosit corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati accusantium repellendus voluptatum reprehenderit deleniti cupiditate ad fugit illo blanditiis nisi?",
+    desc: "Cats are not just pets; they're cherished friends who bring warmth and companionship into our lives. Also they have an uncanny ability to understand our emotions, offering silent support and unwavering loyalty as we navigate life's journey together.",
   },
   {
     id: 2,
     icon: faPaw,
     title: "A Lovely Place",
-    desc: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iustosit corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati accusantium repellendus voluptatum reprehenderit deleniti cupiditate ad fugit illo blanditiis nisi?",
+    desc: "Their peaceful demeanor and serene demeanor turn even the simplest of spaces into a haven of relaxation and tranquility. In the company of a beloved cat, any place becomes a lovely retreat from the chaos of the world.",
   },
   {
     id: 3,
     icon: faBowlFood,
     title: "A Lovely Meal",
-    desc: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iustosit corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati accusantium repellendus voluptatum reprehenderit deleniti cupiditate ad fugit illo blanditiis nisi?",
+    desc: "The sound of kibble clinking against the bowl or the aroma of freshly opened wet food is enough to make their tails wag with excitement. Sharing a meal with a beloved cat is a reminder to savor life's simple pleasures and enjoy each moment to the fullest.",
   },
   {
     id: 4,
     icon: faHeart,
     title: "A Lovely Day",
-    desc: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iustosit corrupti? Distinctio cumque, itaque dicta nemo ipsa obcaecati accusantium repellendus voluptatum reprehenderit deleniti cupiditate ad fugit illo blanditiis nisi?",
+    desc: "From the moment they greet us with gentle meows in the morning until they curl up beside us at night, cats infuse each day with love and light. Their playful antics and endearing quirks bring joy and laughter into our lives, turning ordinary moments into extraordinary memories. ",
   },
 ];
 
