d4c102b248148cf8e9fd80b5f108914c9a02d222
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-02 14:02:18 +0200

    Update ReadMore.vue

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -1,36 +1,34 @@
 <template>
   <div
     id="purpose"
-    className="max-w-[1400px] m-auto py-16 grid lg:grid-cols-3 md:grid-cols-2 lg:gap-32"
+    className="max-w-[1800px] m-auto py-16 grid lg:grid-cols-3 md:grid-cols-2 md:gap-5"
   >
     <div>
       <img
         src="https://m.media-amazon.com/images/M/MV5BOWI3NzQ0MTAtMGQzNi00NjY5LTk5YTYtZjE0M2EzODlkNGRhXkEyXkFqcGdeQXVyMjAwNzczNTU@._V1_.jpg"
         alt="Enigmatic Cat"
-        className="lg:min-w-1/4 md:w-[380px] md:min-h-[490px]  m-10 sm:w-[60svw] xs:w-[60svw] mx-auto   md:mt-20 lg:mt-14 sm:mt-0 shadow-2xl shadow-hero hover:scale-110 transition-all delay-75"
+        className="lg:min-w-[380px] lg:h-[550px]  md:w-[380px] md:h-[520px]  sm:w-[60svw] w-[60svw]  mx-auto  md:mt-36  sm:mt-0 shadow-2xl shadow-hero hover:scale-110 transition-all delay-75"
       />
     </div>
-    <div
-      className="flex text-justify flex-col lg:mt-0 md:mt-20 lg:p-0 md:p-0 sm:p-5"
-    >
+    <div className="flex flex-col text-justify lg:ml-8 mt-20 md:p-0 p-5">
       <p
-        className="underline lg:text-[20px] md:text-[17px] xs:text-[17px] xs:mt-10 lg:mt-0 md:mt-0 sm:mt-10 font-bold cursor-pointer"
+        className="underline md:text-[20px] text-[17px] font-bold cursor-pointer "
       >
         Check this out
       </p>
       <h1
-        className="lg:text-3xl md:text-[25px] xs:text-lg sm:text-2xl mt-2 text-start  "
+        className="lg:text-3xl md:text-[25px] text-lg sm:text-2xl mt-2 text-start  "
       >
         <b>Captivating Creatures:</b> The Enigmatic Charm of Cats
       </h1>
-      <p className="flex mt-5 md:text-[15px] lg:text-[17px] p-2">
+      <p className=" mt-5 md:text-lg md:p-3 p-0 ">
         Cats, with their enigmatic gaze and graceful movements, possess an
         undeniable allure that has captivated humans for centuries. These
         enigmatic felines, revered in ancient cultures and cherished as beloved
         companions in modern times, embody a unique blend of independence and
         affection.
       </p>
-      <div className="flex my-5 md:text-[15px] lg:text-[17px] pr-2">
+      <div className="flex my-5 md:text-[17px] md:pr-3 p-0">
         <font-awesome-icon
           class="mx-3 my-1 hover:text-babyblue2 delay-75 cursor-pointer"
           :icon="faArrowRight"
@@ -40,7 +38,7 @@
           every step.
         </p>
       </div>
-      <div className="flex  md:text-[15px] lg:text-[17px] pr-2">
+      <div className="flex my-5 md:text-[17px] md:pr-3 p-0">
         <font-awesome-icon
           class="mx-3 my-1 hover:text-babyblue2 delay-75 cursor-pointer"
           :icon="faArrowRight"
@@ -50,7 +48,7 @@
           intrigues and delights.
         </p>
       </div>
-      <div className="flex my-5 md:text-[15px] lg:text-[17px] pr-2">
+      <div className="flex my-5 md:text-[17px] md:pr-3 p-0">
         <font-awesome-icon
           class="mx-3 my-1 hover:text-babyblue2 delay-75 cursor-pointer"
           :icon="faArrowRight"
@@ -60,7 +58,7 @@
           tapestry of charm into the fabric of our lives.
         </p>
       </div>
-      <div className="flex  md:text-[15px] lg:text-[17px] pr-2">
+      <div className="flex my-5 md:text-[17px] md:pr-3 p-0">
         <font-awesome-icon
           class="mx-3 my-1 hover:text-babyblue2 delay-75 cursor-pointer"
           :icon="faArrowRight"
@@ -71,29 +69,27 @@
         </p>
       </div>
       <button
-        className="bg-hero2 hover:bg-hero transition duration-3000 ease-out delay-100 rounded w-32 h-12 text-white font-bold md:mt-8 lg:mt-10 sm:mt-5 xs:mt-5 xs:mx-auto sm:mx-auto md:mx-0 lg:mx-0 "
+        className="bg-hero2 hover:bg-hero transition duration-3000 ease-out delay-100 rounded w-32 h-12 text-white font-bold md:mt-8 lg:mt-10 sm:mt-5 mt-5 mx-auto  md:mx-0 lg:mx-0"
       >
         Read More
       </button>
     </div>
-    <div className="  md:mt-12 lg:mt-0 sm:mt-0  xs:mt-14 mx-auto text-center ">
-      <h1
-        className="lg:text-2xl md:text-[25px] sm:text-xl xs:text-xl font-bold"
-      >
+    <div className=" mt-12 mx-auto text-center ">
+      <h1 className="lg:text-2xl md:text-[25px]  text-xl  font-bold">
         Also here is what our users said...
       </h1>
       <div
-        className="grid lg:grid-cols-1 md:grid-cols-3 lg:w-[20svw] md:w-[100svw] sm:w-[60svw] xs:w-[60svw] gap-6 mt-6 mx-auto "
+        className="grid lg:grid-cols-1 md:grid-cols-3 lg:w-[20svw] md:w-[100svw] w-[60svw] gap-6 mt-6 mx-auto "
       >
         <div
           v-for="comment in comments"
           :key="comment.id"
-          className="border-8  h-auto border-babyblue rounded hover:scale-110 hover:border-babyblue2  transition-all delay-75 cursor-pointer"
+          className="border-8  h-auto border-babyblue rounded hover:scale-110 hover:border-babyblue2   transition-all delay-75 cursor-pointer"
         >
           <p className="text-lg md:text-xl font-bold mt-4">
             {{ comment.title }}
           </p>
-          <p className="text-lg md:text-xl p-3 text-justify  ">
+          <p className="text-lg md:text-xl p-3 text-justify   ">
             {{ comment.desc }}
           </p>
         </div>
