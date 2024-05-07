ea2ed7f38d1ca4fbedc3410ea3bf5e587051612b
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-07 14:45:04 +0200

    added transparent bg

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,64 +1,67 @@
 <template>
-  <div
-    class="w-full min-h-[50px] bg-hero flex justify-between items-center absolute z-10 text-white"
-    :class="{ fixed: open }"
-  >
+  <div>
     <div
-      class="md:pl-16 pl-5 text-2xl text-gray-400 hover:text-white hover:scale-110 transition-all delay-75 cursor-pointer"
+      class="w-full min-h-[50px] bg-hero flex justify-between items-center absolute z-10 text-white"
+      :class="{ fixed: open }"
     >
-      LITTLE PAWS
-      <font-awesome-icon class="icon" :icon="faCat" />
-    </div>
-    <div class="md:hidden relative z-10">
-      <button @click="handleClick" class="text-xl text-gray-400 mr-4">
-        <font-awesome-icon :icon="open ? faTimes : faBars" />
-      </button>
-    </div>
-    <ul
-      class="md:flex"
-      :class="[
-        open ? 'block' : 'hidden',
-        'md:h-[50px]',
-        'h-screen',
-        'md:mt-0',
-        'absolute',
-        'md:relative',
-        'top-0',
-        'right-0',
-        'md:top-0',
-        'md:right-auto',
-        'flex-col',
-        'md:flex-row',
-        'text-gray-400',
-        'text-xl',
-        'bg-hero2',
-        'md:bg-transparent',
-        'w-[30vw]',
-        'md:w-auto',
-        'text-center',
-        'justify-center',
-        'items-center',
-      ]"
-    >
-      <li
-        class="p-4 hover:text-white hover:underline mt-10 md:mt-0"
-        @click="handleClick"
+      <div
+        class="md:pl-16 pl-5 text-2xl text-gray-400 hover:text-white hover:scale-110 transition-all delay-75 cursor-pointer"
       >
-        <a href="/">Home</a>
-      </li>
-      <li class="p-4 hover:text-white hover:underline" @click="handleClick">
-        <a href="#purpose">About</a>
-      </li>
-      <li class="p-4 hover:text-white hover:underline" @click="handleClick">
-        <a href="#info">Info</a>
-      </li>
-      <li class="p-4 hover:text-white hover:underline" @click="handleClick">
-        <a href="#gallery">Gallery</a>
-      </li>
-      <li class="p-4 hover:text-white hover:underline" @click="handleClick">
-        <a href="#contact">Contact</a>
-      </li>
-    </ul>
+        LITTLE PAWS
+        <font-awesome-icon class="icon" :icon="faCat" />
+      </div>
+      <div class="md:hidden relative z-10">
+        <button @click="handleClick" class="text-xl text-gray-400 mr-4">
+          <font-awesome-icon :icon="open ? faTimes : faBars" />
+        </button>
+      </div>
+      <ul
+        class="md:flex"
+        :class="[
+          open ? 'block' : 'hidden',
+          'md:h-[50px]',
+          'h-screen',
+          'md:mt-0',
+          'absolute',
+          'md:relative',
+          'top-0',
+          'right-0',
+          'md:top-0',
+          'md:right-auto',
+          'flex-col',
+          'md:flex-row',
+          'text-gray-400',
+          'text-xl',
+          'bg-hero2',
+          'md:bg-transparent',
+          'w-[30vw]',
+          'md:w-auto',
+          'text-center',
+          'justify-center',
+          'items-center',
+        ]"
+      >
+        <li
+          class="p-4 hover:text-white hover:underline mt-10 md:mt-0"
+          @click="handleClick"
+        >
+          <a href="/">Home</a>
+        </li>
+        <li class="p-4 hover:text-white hover:underline" @click="handleClick">
+          <a href="#purpose">About</a>
+        </li>
+        <li class="p-4 hover:text-white hover:underline" @click="handleClick">
+          <a href="#info">Info</a>
+        </li>
+        <li class="p-4 hover:text-white hover:underline" @click="handleClick">
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li class="p-4 hover:text-white hover:underline" @click="handleClick">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+    <div :class="{ darkbg: open }"></div>
   </div>
 </template>
 
@@ -98,8 +101,12 @@ export default {
 <style scoped>
 .fixed {
   position: fixed;
-  top: 0;
-  left: 0;
+}
+.darkbg {
+  position: fixed;
   width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9;
 }
 </style>
